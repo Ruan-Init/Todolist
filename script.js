1e2f50c1f8f6c6f1c5290a89e9cc99ed0d1d7f0f
Ruan-init <ruan.espindola17@gmail.com>
2024-03-02 00:26:17 -0300

    Projeto todolist

---
+++ script.js
@@ -0,0 +1,19 @@
+    const buttonNewevent = document.querySelector(".button-newevent");
+    const buttonNew = document.querySelector(".button-new");
+    const buttonAdd = document.querySelector(".button-add");
+
+
+
+    buttonNewevent.addEventListener("click", function() {
+         console.log("New Event");
+    });
+    
+    buttonNew.addEventListener("click", function() {
+        console.log("New");
+    });
+
+    buttonAdd.addEventListener("click", function(){
+        console.log("+Add")
+    });
+    
+   
